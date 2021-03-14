d9e3785612a298903a2820dc63e76d936cfda242
Aidan Samuelson <aidan.samuelson@wustl.edu>
2021-03-14 15:37:52 -0500

    sticky

--- scripts/ts/dynamic-dom/sticky.js
+++ scripts/ts/dynamic-dom/sticky.js
@@ -1,11 +1,19 @@
+
+
 export function setupSticky() {
     // TODO: Copy and paste your sticky code in here!!
     window.onscroll = function() {myFunction()};
-    
     var header = document.getElementById("main-menu-container");
     var sticky = header.offsetTop;
+    var drop = document.getElementById("mturk-top-banner-drop-down-content");
     
     function myFunction() {
+      if (window.getComputedStyle(drop).display === "none") {
+        header.classList.add("sticky2");
+      } else {
+        header.classList.remove("sticky2");
+      }
+
       if (window.pageYOffset > sticky) {
         header.classList.add("sticky");
       } else {
